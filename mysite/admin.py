361f91b91b8e7f52e32e3444bf528970bfee848b
yuchi05 <11056023@ntub.edu.tw>
2023-10-04 15:51:44 +0800

    新增Post物件跟管理介面

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from mysite.models import Post
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pub_date') #加上標題 網址名稱 時間
+    
+admin.site.register(Post,PostAdmin)
--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    slug = models.CharField(max_length = 200)
+    body = models.TextField() #body是不限制長度的欄位
+    pub_date = models.DateTimeField(auto_now_add=True) #自動取得現在時間
+    
+    class Meta:
+        ordering = ('-pub_date', ) #反向排序(大排到小) pub_date名字需相同
+    
+    def __str__(self) -> str:
+        return self.title
+    
