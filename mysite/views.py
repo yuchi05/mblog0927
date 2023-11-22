989c15443e0c8e58e5f8064fcab7f3e92c1b6bfa
yuchi05 <11056023@ntub.edu.tw>
2023-11-22 16:44:44 +0800

    1122

--- mysite/views.py
+++ mysite/views.py
@@ -5,15 +5,44 @@ from datetime import datetime
 from django.shortcuts import redirect
 
 
-def homepage(request):
+def homepage(request): #首頁
     posts = Post.objects.all()
     now = datetime.now()
+    hour = now.timetuple().tm_hour #時間 index用到hour
     return render(request, 'index.html', locals()) #透過render呼叫網站
 
 def showpost(request, slug):
     post = Post.objects.get(slug=slug)    #select * from post where slug=%slug
     return render(request, 'post.html', locals())
-    
+
+def carlist(request, maker=0): 
+    # '''
+    # car_maker = ['SAAB', 'Ford', 'Honda', 'Mazda', 'Nissan','Toyota' ]
+    # car_list = [ 
+    #             [],
+    #             ['Fiesta', 'Focus', 'Modeo', 'EcoSport', 'Kuga', 'Mustang'],
+    #             ['Fit', 'Odyssey', 'CR-V', 'City', 'NSX'],
+    #             ['Mazda3', 'Mazda5', 'Mazda6', 'CX-3', 'CX-5', 'MX-5'],
+    #             ['Tida', 'March', 'Livina', 'Sentra', 'Teana', 'X-Trail', 'Juke', 'Murano'],
+    #             ['Camry','Altis','Yaris','86','Prius','Vios', 'RAV4', 'Wish']
+    #             ]
+    # '''
+    car_maker = ['Ford', 'Honda', 'Mazda']
+    car_list = [[ {'model':'Fiesta', 'price': 203500},
+                    {'model':'Focus','price': 605000},
+                    {'model':'Mustang','price': 900000}],
+                [{'model':'Fit', 'price': 450000},
+                    {'model':'City', 'price': 150000},
+                    {'model':'NSX', 'price':1200000}],
+                [{'model':'Mazda3', 'price': 329999},
+                    {'model':'Mazda5', 'price': 603000},
+                    {'model':'Mazda6', 'price':850000}],]
+    maker = maker
+    maker_name =  car_maker[maker]
+    cars = car_list[maker]
+    return render(request, 'carlist.html', locals())
+
+
 # def about(request, num=-1):
 #     mthml=f'''
 #     <hmtl>
